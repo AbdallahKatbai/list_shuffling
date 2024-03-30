bb9e3f7acd02f4c97ffd26478f96e3df177d8f60
Abdallah-Katbai <abdallah.katbai@gmail.com>
2024-03-30 14:10:38 +0200

    added the print_nmbrs fn and the additions2 list

--- main.py
+++ main.py
@@ -22,10 +22,27 @@ subtractions1 = ['10-1=', '10-2=', '10-3=', '10-4=', '10-5=', '10-6=', '10-7=', 
                        '2-1=', '2-2=',
                        '1-1=']
 
+additions2 = ['5+5=', '5+6=', '5+7=', '5+8=', '5+9=',
+              '6+5=', '6+6=', '6+7=', '6+8=', '6+9=',
+              '7+5=', '7+6=', '7+7=', '7+8=', '7+9=',
+              '8+5=', '8+6=', '8+7=', '8+8=', '8+9=',
+              '9+5=', '9+6=', '9+7=', '9+8=', '9+9=',
+              ]
 
-def print_list(operations_list):
-    for i in range(len(operations_list)):
+
+def print_list(operations_list, r=1):
+    for i in range(len(operations_list)//r):
         print(operations_list[i] + '.')
+
+
+def print_nmbrs(n):
+    nmbrs = []
+    for i in range(1, n):
+        nmbrs.append(i)
+    random.shuffle(nmbrs)
+    for i in range(len(nmbrs)):
+        print(nmbrs[i], '=> ')
+    print('___________________')
 
 
 def solve_list(operations_list):
@@ -46,4 +63,6 @@ def give_multiple_examples(operations):  #'operations' is list
         give_examples(i)
 
 
-give_multiple_examples([subtractions1, additions1])
+#give_multiple_examples([subtractions, additions])
+print_nmbrs(10)
+give_multiple_examples([additions2])
